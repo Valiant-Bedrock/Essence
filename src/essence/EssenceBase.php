67be4e96eb2bd464e5614235873e67826dc519ec
Matthew Jordan <mattjordanbusiness@gmail.com>
2023-06-24 09:14:14 -0500

    feat: initial commit

---
+++ src/essence/EssenceBase.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (C) 2020 - 2023 | Valiant Network
+ *
+ * This program is private software. You may not redistribute this software, or
+ * any derivative works of this software, in source or binary form, without
+ * the express permission of the owner.
+ *
+ * @author sylvrs
+ */
+declare(strict_types=1);
+
+namespace essence;
+
+use pocketmine\plugin\PluginBase;
+
+final class EssenceBase extends PluginBase {
+
+	protected function onEnable(): void {
+	}
+
+	protected function onDisable(): void {
+	}
+}
