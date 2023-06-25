7c2964c7ad2506081a2a1b435ddd384345edc674
Matthew Jordan <mattjordanbusiness@gmail.com>
2023-06-24 21:30:04 -0500

    [deploy] feat: push associated key and factory classes

---
+++ src/essence/EssenceDatabaseKeys.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Copyright (C) 2020 - 2023 | Valiant Network
+ *
+ * This program is private software. You may not redistribute this software, or
+ * any derivative works of this software, in source or binary form, without
+ * the express permission of the owner.
+ *
+ * @author sylvrs
+ */
+
+namespace essence;
+
+final class EssenceDatabaseKeys {
+	public const PLAYER_LOAD = "player.load";
+	public const PLAYER_LOAD_BY_USERNAME = "player.load_by_username";
+	public const PLAYER_SAVE = "player.save";
+
+	private function __construct() {
+	}
+}
